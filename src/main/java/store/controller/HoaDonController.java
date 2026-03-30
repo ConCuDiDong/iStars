1bc9474ac174e04b861aa6c8f336ca7ac13d0f95
Hanh Yen <phamtrinhthihanhyen@gmail.com>
2026-03-30 14:47:19 +0700

    Them hoa don + hoa don chi tiet

---
+++ src/main/java/store/controller/HoaDonController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package store.controller;
+
+import store.entity.HoaDon;
+
+/**
+ *
+ * @author LEGION
+ */
+public interface HoaDonController extends CrudController<HoaDon>{
+    
+}
